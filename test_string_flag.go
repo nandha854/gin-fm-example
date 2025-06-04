f574a60e31a26dd7140de6744a1157743bcc2102
nandha854 <nmurugan@cloudbees.com>
2025-06-04 22:04:12 +0530

    Remove feature flag: WelcomeMessage

--- test_string_flag.go
+++ test_string_flag.go
@@ -12,7 +12,6 @@ import (
 
 // Define a struct to hold feature flags
 type AppConfig struct {
-WelcomeMessage *rox.String
 DashboardTheme *rox.String
 }
 
@@ -28,7 +27,6 @@ os.Exit(1)
 
 // Define the feature flags with default values and allowed options
 config = AppConfig{
-	WelcomeMessage: rox.NewString("Hello!", []string{"Hello!", "Hi!", "Greetings!"}),
 	DashboardTheme: rox.NewString("light", []string{"light", "dark", "synthwave"}),
 }
 
@@ -53,7 +51,7 @@ context.SetCustomStringProperty("user_id", "123")
 context.SetCustomStringProperty("user_group", "beta-users")
 
 // Evaluate each feature flag in the current context
-welcome := config.WelcomeMessage.GetValue(context)
+welcome := "Hii!"
 theme := config.DashboardTheme.GetValue(context)
 
 fmt.Println("Welcome Message:", welcome)
@@ -62,4 +60,3 @@ fmt.Println("Dashboard Theme:", theme)
 // Keep running so updates can be pulled dynamically
 select {}
 }
-
